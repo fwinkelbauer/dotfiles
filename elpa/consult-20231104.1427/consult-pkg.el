ded05b2152dbbc6aa8d8708a18f272bac315b6da
Florian Winkelbauer <mail@florianwinkelbauer.com>
2023-11-05 08:37:51 +0100

    Upgrade packages

---
+++ elpa/consult-20231104.1427/consult-pkg.el
@@ -0,0 +1,15 @@
+(define-package "consult" "20231104.1427" "Consulting completing-read"
+  '((emacs "27.1")
+    (compat "29.1.4.1"))
+  :commit "1ae8945dbec77fc01a33b3b32e3b74fd8eaf45fe" :authors
+  '(("Daniel Mendler and Consult contributors"))
+  :maintainers
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :keywords
+  '("matching" "files" "completion")
+  :url "https://github.com/minad/consult")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
