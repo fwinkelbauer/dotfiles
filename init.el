75f7901e77ebfe68360da7831f674caa1c00b533
Florian Winkelbauer <mail@florianwinkelbauer.com>
2023-11-16 20:43:38 +0100

    Remove custom safe-local-variable-values

--- init.el
+++ init.el
@@ -13,8 +13,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(embark-consult embark orderless vertico consult csharp-mode powershell doom-modeline company elfeed color-theme-sanityinc-tomorrow markdown-mode magit))
- '(safe-local-variable-values '((eval flyspell-buffer))))
+   '(embark-consult embark orderless vertico consult csharp-mode powershell doom-modeline company elfeed color-theme-sanityinc-tomorrow markdown-mode magit)))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
