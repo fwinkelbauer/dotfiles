e3c1fa57177916eb0e2e33b6d84e2a05305de925
Florian Winkelbauer <mail@florianwinkelbauer.com>
2023-12-05 19:06:42 +0100

    Move custom-set-variables and custom-set-faces

--- init.el
+++ init.el
@@ -6,18 +6,3 @@
 
 (prefer-coding-system 'utf-8)
 (org-babel-load-file (locate-user-emacs-file "emacs.org"))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(nerd-icons-dired embark-consult embark orderless vertico consult powershell doom-modeline company elfeed color-theme-sanityinc-tomorrow markdown-mode magit)))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
