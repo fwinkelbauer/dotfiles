788b030dd7b04b818e00d685b8325aad151f6310
Florian Winkelbauer <mail@florianwinkelbauer.com>
2023-12-05 19:15:55 +0100

    Move package-initialize

--- init.el
+++ init.el
@@ -1,8 +1,2 @@
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
-
-(package-initialize)
-
 (prefer-coding-system 'utf-8)
 (org-babel-load-file (locate-user-emacs-file "emacs.org"))
